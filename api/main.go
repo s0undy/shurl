26c280355b0c33ab316a91eacaace1dfa4c9ac21
s0undy <antonjosefsson@hotmail.se>
2023-09-10 10:14:45 +0200

    Outlines of API/Websrv

--- api/main.go
+++ api/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/s0undy/shurl/database"
+	"github.com/s0undy/shurl/routes"
 )
 
+func SetupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortURL)
+}
+
 func main() {
+	log.Println("Loading .env")
 	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal(err)
@@ -18,5 +27,10 @@ func main() {
 	if err != nil {
 		log.Fatal("Could not connect to DB")
 	}
-	fmt.Println(db.Config)
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	SetupRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
---
+++ api/routes/short.go
@@ -0,0 +1,7 @@
+package routes
+
+import "github.com/gofiber/fiber"
+
+func ShortURL(c *fiber.Ctx) error {
+
+}
